e60aa85d70dd9a6c3479697bdafbcef8f5898cfa
Pj Malva <pjmalva@gmail.com>
2025-08-07 09:54:52 -0300

    a

--- src/lib/components/assistir/lesson.svelte
+++ src/lib/components/assistir/lesson.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+    import { CircleCheck, Play } from "lucide-svelte";
+
     let { lesson } = $props();
 </script>
 
-<div class="flex flex-col gap-2 w-full">
-    <div>Titulo da aula</div>
+<div 
+    class="grid gap-2 w-full border-b border-gray-300 py-2"
+    style="grid-template-columns: 35px 1fr 20px;"
+>
+    <div class="flex items-center justify-center w-full">
+        <div class="bg-slate-600 rounded h-full font-bold flex flex-col items-center justify-center gap-1 w-full">
+            <div class="text-xl">1</div>
+            <CircleCheck class="w-4 h-4 text-green-500" />
+        </div>
+    </div>
+    <div>
+        <div class="min-h-12">
+            {lesson.name}
+        </div>
+        <div>
+            <span class="bg-slate-600 rounded p-1 text-xs">{lesson.time}min</span>
+        </div>
+    </div>
+    <div class="flex items-end">
+        <Play class="w-4 h-4" />
+    </div>
 </div>
 
--- src/lib/components/assistir/modules.svelte
+++ src/lib/components/assistir/modules.svelte
@@ -4,6 +4,8 @@
     import Lesson from "$lib/components/assistir/lesson.svelte";
     import Notes from "$lib/components/assistir/notes.svelte";
     import Complements from "$lib/components/assistir/complements.svelte";
+    import Button from "../ui/button/button.svelte";
+    import { ChevronsRightIcon, CircleCheck } from "lucide-svelte";
 
     let { modules } = $props();
 </script>
@@ -21,27 +23,52 @@
         </Tabs.List>
         {#each modules as module}
             <Tabs.Content value={module.id}>
-                <div class="flex flex-col gap-2 w-full p-8 bg-slate-100">
+                <div class="flex flex-col gap-2 w-full p-8 bg-gray-100 rounded-sm">
                     <div class="flex flex-col gap-2 w-full">
-                        <div>Titulo da aula</div>
+                        <div class="font-bold text-lg mb-2">
+                            <span class="bg-slate-800 text-white p-2 rounded">1</span> 
+                            Titulo da aula
+                        </div>
 
-                        <div class="grid grid-cols-3 bg-slate-800 text-white">
-                            <div class="col-span-2">
+                        <div 
+                            class="grid grid-cols-3 bg-slate-800 text-white rounded h-[500px]"
+                        >
+                            <div class="col-span-2 bg-gray-900 rounded-l">
                                 <Video />
                             </div>
 
-                            <div class="col-span-1">
-                                {#each module.lessons as lesson}
-                                    <Lesson lesson={lesson} />
-                                {/each}
+                            <div class="col-span-1 p-4 grid" style="grid-template-rows: 1fr 20px;">
+                                <div class="">
+                                    {#each module.lessons as lesson}
+                                        <Lesson lesson={lesson} />
+                                    {/each}
+                                </div>
+
+                                <div class="text-sm pt-[7px] font-bold text-white text-end border-t border-slate-600">
+                                    Aulas assistidas 1/9
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <Complements />
 
-                    <div class="flex justify-center items-center w-full h-full">
-                        Botão de concluir e prosseguir
+                    <div class="flex justify-between items-center w-full h-full">
+                        <div>
+                            <Button variant="default">
+                                <CircleCheck class="w-4 h-4" />
+                                Concluir Aula
+                            </Button>
+                        </div>
+
+                        <div>
+                            <Button variant="secondary">
+                                Proxima Aula
+                                <ChevronsRightIcon class="w-4 h-4" />
+                            </Button>
+                        </div>
+
+                        <div></div>
                     </div>
 
                     <Notes />
